6dbaef242c257e816bef357ac472938dff69e747
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2017-06-13 19:15:13 -0300

    added search's mecanisme and other components

--- cinemao/app/app.js
+++ cinemao/app/app.js
@@ -3,12 +3,35 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'myApp',
   'myApp.view1',
   'myApp.view2',
+  'myApp.busca',
   'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+])
+
+
+.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
+
+}])
+
+.run(['$rootScope', '$location', function($rootScope, $location) {
+  $rootScope.$on('$stateChangeError',
+        function (event, toState, toParams, fromState, fromParams, error) {
+            console.log(event, error);
+        });
+
+  $rootScope.buscar = function(tag){
+
+    $location.path('/busca/'+tag);
+    $rootScope.tag = null;
+   
+
+  };      
+
 }]);
+
+
