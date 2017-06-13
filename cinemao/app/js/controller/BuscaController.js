f88c02a19415c30ff7c4930cfebed243749d5674
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2017-06-13 20:25:10 -0300

    supercommit

--- cinemao/app/js/controller/BuscaController.js
+++ cinemao/app/js/controller/BuscaController.js
@@ -21,6 +21,11 @@ angular.module('myApp.busca', ['ngRoute'])
     console.log(tag);
     $scope.tag = tag; 
 
+    $scope.urlData =[ "https://images-na.ssl-images-amazon.com/images/M/MV5BOTA4MTE3MTQwMF5BMl5BanBnXkFtZTgwNzk4MTg4MTI@._V1_SX300.jpg", 
+    "http://3milliondogs.com/blog-assets-two/2015/03/goldenlab.jpg",
+     "http://legacy.semantic-ui.com/images/demo/photo.jpg",
+     "https://img.clasf.co.ve/2016/03/27/regalo-cachorros-siberian-husky-cachorros-jose-antonio-paez-201603271602546431020000.jpg"];
+
 
 }]);
 
